9139e873cafbb4ee8ce6670a89aa07d3195ab04d
Miguel Isaac <mikik@hotmail.es>
2015-09-09 15:44:45 -0500

    Create config.lua
    configuration to the raccoon

---
+++ config.lua
@@ -0,0 +1,11 @@
+racoon = {}
+
+--This grants and revokes the fast priv along with fly when it is set to true.
+--If you grant the fast priv on your server, do *not* enable it.
+--As when the effect wears off, the fast priv of the player will go as well.
+racoon.fast = false
+
+racoon.time = 600
+
+--If you want raccoon to be avalible only through /giveme or at admin shops, set this to false
+racoon.crafts = true
